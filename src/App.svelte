cc963b8cb134798c5fe3439cfc2c7d69b9b4171f
alex <gaunyu.wu@orange.com>
2021-06-11 00:33:58 +0800

    1-add-css

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,8 @@
-<script>
-	export let name;
-</script>
+<h1>test12123</h1>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- 1 add google fonts  -->
+<!-- 2 sudo killall node and check the result in browser after run npm run dev  -->
+<!-- 3 add blueprint-css  npm  i blueprint-css -D -->
+<!-- 4 npm install --save-dev rollup-plugin-css-only -->
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- 5 config rollup.config.js -->
